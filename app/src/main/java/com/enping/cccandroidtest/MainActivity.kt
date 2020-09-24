20c730d3fd46994b173cb18a48f7ab7da250841a
Hsieh Enping <shieh.npin@gmail.com>
2020-09-25 00:00:22 +0800

    Add better logger.

--- app/src/main/java/com/enping/cccandroidtest/MainActivity.kt
+++ app/src/main/java/com/enping/cccandroidtest/MainActivity.kt
@@ -2,11 +2,15 @@ package com.enping.cccandroidtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
+import timber.log.Timber.DebugTree
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Timber.plant(DebugTree())
     }
 }
